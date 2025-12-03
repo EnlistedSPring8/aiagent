b599a59c6959e4edc814e57f03547ffa941ea87c
EnlistedSPring8 <onnzki@gmail.com>
2025-12-03 19:18:23 +0200

    E: Added usage_metadata to the response

--- main.py
+++ main.py
@@ -3,18 +3,28 @@ from google import genai
 from dotenv import load_dotenv
 
 def main():
-    if api_key == None:
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    if not api_key:
         raise RuntimeError("GEMINI_API_KEY is not set")
-    print("Hello from ai-agent!")
-    client.models.generate_content(
-        model="gemini-1.5-flash",
-        conents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     )
+    if not response.usage_metadata:
+        raise RuntimeError("No usage metadata found in the response")
+    print("User prompt: Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    print("Prompt tokens: " + str(response.usage_metadata.prompt_token_count))
+    print("Response tokens: " + str(response.usage_metadata.candidates_token_count))
+    print("Response:" + "/n" + response.text)
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
 
-client = genai.Client(api_key=api_key)
+
+
 
 if __name__ == "__main__":
     main()
