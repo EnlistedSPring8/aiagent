4f10e8d42b1d2f356516ae42ae0d4d1c3e3b3697
EnlistedSPring8 <onnzki@gmail.com>
2025-12-03 19:38:21 +0200

    F: Added verbose argument

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import argparse
+from google.genai import types
 from google import genai
 from dotenv import load_dotenv
 
@@ -11,16 +13,24 @@ def main():
     
     client = genai.Client(api_key=api_key)
 
+    parser = argparse.ArgumentParser(description="Generate content using Gemini API")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    parser.add_argument("prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.prompt)])]
+
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=messages
     )
+    
     if not response.usage_metadata:
         raise RuntimeError("No usage metadata found in the response")
-    print("User prompt: Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-    print("Prompt tokens: " + str(response.usage_metadata.prompt_token_count))
-    print("Response tokens: " + str(response.usage_metadata.candidates_token_count))
-    print("Response:" + "/n" + response.text)
+    if args.verbose is True:
+        print(f"User Prompt: {args.prompt}")
+        print("Prompt tokens: " + str(response.usage_metadata.prompt_token_count))
+        print("Response tokens: " + str(response.usage_metadata.candidates_token_count))
+    print("Response: " + "\n" + response.text)
 
 
 
